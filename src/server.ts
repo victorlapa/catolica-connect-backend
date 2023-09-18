ad209af7e37b98b388d3ac436b3dba013b0255f0
Victor Lapa <victtorlapa1@gmail.com>
2023-09-18 20:31:42 -0300

    post schema

--- src/server.ts
+++ src/server.ts
@@ -17,19 +17,43 @@ app.post("/users", async (request, reply) => {
     name: z.string(),
     email: z.string().email(),
     tag: z.string().min(4).max(18),
+    curso: z.string(),
+    semestre: z.number().min(1).max(10),
   });
 
-  const { name, email, tag } = createUserSchema.parse(request.body);
+  const { name, email, tag, curso, semestre } = createUserSchema.parse(
+    request.body
+  );
+
+  if (email.endsWith("@catolicasc.edu.br") === false) {
+    return reply.status(400).send("Apenas emails CatolicaSC são permitidos");
+  }
 
   await prisma.user.create({
     data: {
       name,
       email,
       tag,
+      curso,
+      semestre,
     },
   });
 
   return reply.status(201).send();
+});
+
+app.get("/posts", async () => {
+  const posts = await prisma.post.findMany();
+
+  return { posts };
+});
+
+app.post("/posts", async (request, reply) => {
+  const createPostSchema = z.object({
+    content: z.string().min(1).max(120),
+    authorId: z.string(),
+    likes: z.number().default(0),
+  });
 });
 
 app
